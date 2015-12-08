091024b3b3569aa690223da8fdffcb70f9225093
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-12-07 21:14:45 -0800

    Add solution to day 1

---
+++ day1/day1.hs
@@ -0,0 +1,19 @@
+import Data.List
+
+drop' :: [(Integer, Integer)] -> [(Integer, Integer)]
+drop' = dropWhile g
+    where g (x, _) = x /= (-1)
+
+count' :: String -> [(Integer,Integer)]
+count' = foldl' f [(0,0)]
+    where f l@((x,y):_) a = if a == '(' then (x+1,y+1):l else (x-1,y+1):l
+          f [] _ = []
+
+count :: String -> Integer
+count = foldr f 0
+    where f a b = if a == '(' then b+1 else b-1
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    print . snd . head . drop' . reverse . count' $ input
