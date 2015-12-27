4d23800c74be3c018baa41e25131355be4ce94ac
Vivitsu Maharaja <vivitsu.maharaja@gmail.com>
2015-12-26 18:19:56 -0800

    Solve day 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,8 +1,19 @@
 import Data.List.Split
 
+volume :: [Integer] -> Integer
+volume (l:w:h:_) = l*w*h
+volume _ = 0
+
+perimeterElems :: [Integer] -> [Integer]
+perimeterElems (l:w:h:_) = [2*l+2*w, 2*w+2*h, 2*h+2*l]
+perimeterElems _ = []
+
 surfaceAreaElems :: [Integer] -> [Integer]
 surfaceAreaElems (l:w:h:_) = [l*w, w*h, h*l]
 surfaceAreaElems _ = []
+
+ribbon :: [[Integer]] -> [Integer]
+ribbon = map (\ x -> minimum (perimeterElems x) + volume x)
 
 amt :: [[Integer]] -> [Integer]
 amt [] = []
@@ -15,7 +26,14 @@ ints = map (map (read :: String -> Integer))
 dims :: [String] -> [[String]]
 dims = map (splitOn "x")
 
+printWrappingPaper :: String -> IO ()
+printWrappingPaper = print . sum . amt . ints . dims . lines
+
+printRibbon :: String -> IO ()
+printRibbon = print . sum . ribbon . ints . dims . lines
+
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print . sum . amt . ints . dims $ lines input
+    _ <- printWrappingPaper input
+    printRibbon input
